9d66ed341d582094782b07e278f2fefcb39323f8
Fukui <fukujavai@gmail.com>
2023-09-05 15:47:09 +0900

    一番最初のコミット

---
+++ src/main/java/webapp/gooutmap/GoOutMapApplication.java
@@ -0,0 +1,13 @@
+package webapp.gooutmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoOutMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoOutMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webapp/gooutmap/GoOutMapApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.gooutmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoOutMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
