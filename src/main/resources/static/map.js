7003dc424374c51a5020c6f9c5aca91485c01abb
Fukui <fukujavai@gmail.com>
2023-09-11 14:05:49 +0900

    マップ表示で都道府県と市区町村の情報を反映できた

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -1,9 +1,34 @@
 function initMap() {
-    var place = document.getElementById('#pref').value;
-    alert(place);
+    //golistの要素を取得
+    var gotable = document.getElementById("go");
+        var pref = gotable.rows[1].cells[1].innerHTML;
+        var city = gotable.rows[2].cells[1].innerHTML;
+        var genre = gotable.rows[3].cells[1].innerHTML;
+        var move_means = gotable.rows[4].cells[1].innerHTML;
+        console.log(pref,city,genre,move_means);
+    //my_homeの要素を取得
+    var gotable = document.getElementById("home");
+        var latitude = gotable.rows[1].cells[1].innerHTML;
+        var longitude = gotable.rows[2].cells[1].innerHTML;
+        console.log(latitude,longitude);
 
-    new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: { lat: 35.68194580055263, lng: 139.76654773147183 }
-    });
+    //my_homeの要素をジオコード
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: pref + city },
+        function( results, status ){
+            if( status == google.maps.GeocoderStatus.OK ){
+                var aa = results[ 0 ].geometry.location
+                alert(aa);
+                var bb = new google.maps.LatLng(aa);
+                //マップ表示
+                new google.maps.Map(document.getElementById('map'), {
+                zoom: 10,
+                center: bb
+                });
+            }
+            else{
+                alert( 'Faild：' + status );
+            }
+        }
+    );
 }
