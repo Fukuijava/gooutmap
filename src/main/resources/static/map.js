6da95d85c8ed01cd4da0656d6d4dc1a52f3c077e
Fukui <fukujavai@gmail.com>
2023-09-19 10:54:49 +0900

    ルート機能作成中（指定した場所のTypeにマーカーを打てた）

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -15,16 +15,15 @@ function initMap() {
         var move_means = gotable.rows[4].cells[1].innerHTML;
         console.log(pref,city,genre,move_means);
     //my_homeの要素を取得
-    var gotable = document.getElementById("home");
-        var latitude = gotable.rows[1].cells[1].innerHTML;
-        var longitude = gotable.rows[2].cells[1].innerHTML;
-//        var home_la_lo = {
-//            lat: latitude,
-//            lng: longitude
-//        };
-        var home_la_lo = latitude +","+ longitude
-        console.log(home_la_lo);
-        var home = new google.maps.LatLng(home_la_lo);
+    var hometable = document.getElementById("home");
+        var latitude_Str = hometable.rows[1].cells[1].innerHTML;
+        var longitude_Str = hometable.rows[2].cells[1].innerHTML;
+        var latitude_Flo = parseFloat(latitude_Str);
+        var longitude_Flo = parseFloat(longitude_Str);
+        var home = {
+            lat: latitude_Flo,
+            lng: longitude_Flo
+        };
         console.log(home);
 
     //my_homeの要素をジオコード
@@ -46,26 +45,13 @@ function initMap() {
                     zoom: 11,
                     center: pcAddress
                 });
-                bounds.extend(pcAddress); //地図の表示領域を家にする
 
+                bounds.extend(pcAddress); //地図の表示領域を家にする
                 infoWindow.setPosition(home); //ウィンドウを家に置く
                 infoWindow.setContent('my_home'); //ウィンドウにコメントを入れる
                 infoWindow.open(map);//ウィンドウを地図に表示
                 map.setCenter(pcAddress);//地図の中心位置はposの座標 part2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                getNearbyPlaces(pcAddress);//getNearbyPlacesメソッドに都道府県と市区町村の座標を送る
 
 //                new google.maps.Marker({
 //                    position: la_lo,
@@ -73,12 +59,94 @@ function initMap() {
 //                    title: "Hello World!",
 //                });
 //                window.initMap = initMap;
-
             }
             else{
                 alert( 'Faild：' + status );
             }
         }
     );
+}
 
+// 近くの場所の検索リクエストを実行する
+//getNearbyPlacesにリクエストする項目
+//渡された現在位置
+//「google.maps.places.RankBy.DISTANCE」を指定しました。これにより、場所検索に最も近い順に結果が並べ替えられます。
+//radius :中心から半径5000m
+//rankByとradiusはどっちか一つしか指定できない
+//キーワード’sushi’がどこかしらに入ってるやつを探す
+//その場所のタイプで探す
+//keywordとtypesはどっちか一つしか指定できない
+function getNearbyPlaces(pcAddress) {
+    let request = {
+        location: pcAddress,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        //radius: 5000,
+        //keyword: 'sushi'
+        types: ['cafe']
+    };
+    //変数serviceを＃mapのPlaces_apiを使える変数にして、そこでnearbySearchを使ってる
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, nearbyCallback);
+}
+// 近隣検索の結果 (最大 20) を処理します
+//結果を返すやつ
+//最大20回検索に引っかかったやつにマーカーを立ててる
+function nearbyCallback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        createMarkers(results);//メソッド呼び出し
+    }
+}
+
+// 各場所の結果の位置にマーカーを設定します
+function createMarkers(places) {
+    places.forEach(place => {
+        let marker = new google.maps.Marker({
+        position: place.geometry.location,
+        map: map,
+        title: place.name
+    });
+
+    /* クリックリスナーをマーカーに追加する */
+    // 各マーカーにクリックリスナーを追加する
+    //マーカーをクリックした時のイベント処理？
+    google.maps.event.addListener(marker, 'click', () => {
+        let request = {
+            placeId: place.place_id,
+            fields: ['name', 'formatted_address', 'geometry', 'rating','website', 'photos']
+        };
+
+        /* ユーザーがマーカーをクリックしたときにのみ、場所の詳細を取得します。
+        * 結果が得られ次第、すべての場所の詳細を取得する場合
+        * 検索応答によっては、API レート制限に達します。 */
+        service.getDetails(request, (placeResult, status) => {
+            showDetails(placeResult, marker, status)
+        });
+    });
+
+    // このマーカーの位置を含むように地図の境界を調整します
+    bounds.extend(place.geometry.location);
+    });
+    /* すべてのマーカーを配置したら、マップの境界を次のように調整します。
+    * 表示領域内のすべてのマーカーを表示します。 */
+    map.fitBounds(bounds);
+}
+
+/* 情報ウィンドウに場所の詳細を表示する */
+// マーカーの上に詳細を表示する情報ウィンドウを構築します。
+// if (placeResult.rating)     rating = placeResult.rating;は評価があればratingに評価を入れる
+//評価がなければlet rating = "None";のままになる
+function showDetails(placeResult, marker, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        let placeInfowindow = new google.maps.InfoWindow();
+        let rating = "None";
+        if (placeResult.rating)     rating = placeResult.rating;
+        placeInfowindow.setContent
+        ('<div><strong>' + placeResult.name +'</strong><br>' + 'Rating: ' + rating + '</div>');
+        placeInfowindow.open(marker.map, marker);
+        currentInfoWindow.close();
+        currentInfoWindow = placeInfowindow;
+        showPanel(placeResult);
+    } else {
+        console.log('showDetails failed: ' + status);
+    }
 }
